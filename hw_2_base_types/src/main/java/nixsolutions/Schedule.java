dd00f255d1f8d872b8a9e88744fc2fc1f7d29076
Aivynn <aivyn1337@gmail.com>
2022-02-13 02:28:24 +0200

    fixed schedule, not it won't crush with chars in the entered string

--- hw_2_base_types/src/main/java/nixsolutions/Schedule.java
+++ hw_2_base_types/src/main/java/nixsolutions/Schedule.java
@@ -12,27 +12,31 @@ public class Schedule {
 
 
     public static void lessons() throws IOException {
+        try {
+            System.out.println("Enter the number of lessons(from 1 to 10): ");
+           BufferedReader reader = new BufferedReader(
+                   new InputStreamReader(System.in));
+           String name = reader.readLine();
+           int number = Integer.parseInt(name);
+           int sec = 1000;
+           int minute = sec * 60;
+           int hour = minute * 60;
+           int start_lessons = hour * 9;
+           int lesson = 45 * minute;
+           int small = minute * 5 * ((int) Math.ceil((number - 1) / 2f));
+           int big = minute * 15 * (Math.round((number - 1) / 2));
 
-        System.out.println("Enter the string to count when lessons ends");
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
-        String name = reader.readLine();
-        int number = Integer.parseInt(name);
-        int sec = 1000;
-        int minute = sec * 60;
-        int hour = minute * 60;
-        int start_lessons = hour * 9;
-        int lesson = 45 * minute;
-        int small = minute * 5 * ((int) Math.ceil((number - 1) / 2f));
-        int big = minute * 15 * (Math.round((number - 1) / 2));
+           SimpleDateFormat simpleDateFormat =
+                   new SimpleDateFormat("HH:mm:ss");
+           simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        SimpleDateFormat simpleDateFormat =
-                new SimpleDateFormat("HH:mm:ss");
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-        Date date = new Date();
-        date.setTime(start_lessons + (lesson * number) + small + big);
-        System.out.println("Lessons will end at : " + simpleDateFormat.format(date));
-
+           Date date = new Date();
+           date.setTime(start_lessons + (lesson * number) + small + big);
+           System.out.println("Lessons will end at : " + simpleDateFormat.format(date));
+        }
+        catch(Exception e) {
+            System.out.println("Wrong input, your input shouldn't contain chars, try again.");
+            lessons();
+        }
     }
 }
