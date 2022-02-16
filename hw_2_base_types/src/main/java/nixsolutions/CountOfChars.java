c162e335e36f72e013b8d01de646b3e1bcb23048
Aivynn <aivyn1337@gmail.com>
2022-02-16 11:19:05 +0200

    xyz

--- hw_2_base_types/src/main/java/nixsolutions/CountOfChars.java
+++ hw_2_base_types/src/main/java/nixsolutions/CountOfChars.java
@@ -14,65 +14,70 @@ public class CountOfChars {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
-        String name = reader.readLine();
-        SimpleMap buf;
-        int x = 0, t = 1;
-        int count = 0;
-        int ind = 0;
-        int size = 0 ;
-        for (int i = 0; i < name.length(); i++) {
-            if (Character.isLetter(name.charAt(i))) {
-                size++;
+        try{
+            String name = reader.readLine();
+            SimpleMap buf;
+            int x = 0, t = 1;
+            int count = 0;
+            int ind = 0;
+            int size = 0;
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isLetter(name.charAt(i))) {
+                    size++;
+                }
             }
-        }
-        char[] ch = new char[size];
-        for (int i = 0; i < name.length(); i++) {
-            if (Character.isLetter(name.charAt(i))) {
-                ch[ind] = name.charAt(i);
-                ind++;
+            char[] ch = new char[size];
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isLetter(name.charAt(i))) {
+                    ch[ind] = name.charAt(i);
+                    ind++;
+                }
             }
-        }
-        System.out.println(ch.length);
-        StringBuilder unique = new StringBuilder();
-        Arrays.sort(ch);
-        unique.append(ch[x]);
-        boolean flag = false;
+            System.out.println(ch.length);
+            StringBuilder unique = new StringBuilder();
+            Arrays.sort(ch);
+            unique.append(ch[x]);
+            boolean flag = false;
 
-        while (!flag) {
-            if (ch[x] != ch[t]) {
-                x = t;
-                unique.append(ch[x]);
+            while (!flag) {
+                if (ch[x] != ch[t]) {
+                    x = t;
+                    unique.append(ch[x]);
+                }
+                t++;
+                if (t == ch.length) {
+                    flag = true;
+                }
             }
-            t++;
-            if (t == ch.length) {
+
+            flag = false;
+            SimpleMap[] dictArray = new SimpleMap[unique.length()];
+            for (int i = 0; i < unique.length(); i++) {
+                for (char c : ch) {
+                    if (unique.charAt(i) == c) {
+                        count++;
+                    }
+                }
+                dictArray[i] = new SimpleMap(unique.charAt(i), count);
+                count = 0;
+            }
+
+            while (!flag) {
                 flag = true;
-            }
-        }
-
-        flag = false;
-        SimpleMap[] dictArray = new SimpleMap[unique.length()];
-        for(int i = 0;i<unique.length();i++) {
-            for (char c : ch) {
-                if (unique.charAt(i) == c) {
-                    count++;
+                for (int i = 0; i < dictArray.length - 1; i++) {
+                    if (dictArray[i].x < dictArray[i + 1].x) {
+                        flag = false;
+                        buf = dictArray[i];
+                        dictArray[i] = dictArray[i + 1];
+                        dictArray[i + 1] = buf;
+                    }
                 }
             }
-            dictArray[i] = new SimpleMap(unique.charAt(i),count);
-            count = 0;
+            System.out.println(Arrays.toString(dictArray));
         }
-
-        while(!flag) {
-            flag = true;
-            for (int i = 0; i < dictArray.length-1; i++) {
-                if(dictArray[i].x < dictArray[i+1].x)
-                {
-                    flag = false;
-                    buf = dictArray[i];
-                    dictArray[i] = dictArray[i+1];
-                    dictArray[i+1] = buf;
-                }
-            }
+        catch (Exception e) {
+            System.out.println("The string can't be empty, try again.");
+            countChars();
         }
-        System.out.println(Arrays.toString(dictArray));
-    }
+        }
 }
--- hw_2_base_types/src/main/java/nixsolutions/CountOfDigits.java
+++ hw_2_base_types/src/main/java/nixsolutions/CountOfDigits.java
@@ -8,18 +8,18 @@ public class CountOfDigits {
 
     public static void countDigits() throws IOException {
 
-        System.out.println("Enter the string to count digits");
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
+            System.out.println("Enter the string to count digits");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
 
-        String name = reader.readLine();
-        int count = 0;
+            String name = reader.readLine();
+            int count = 0;
 
-        for (int i = 0; i < name.length(); i++) {
-            if (Character.isDigit(name.charAt(i))) {
-                count += Integer.parseInt(String.valueOf(name.charAt(i)));
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isDigit(name.charAt(i))) {
+                    count += Integer.parseInt(String.valueOf(name.charAt(i)));
+                }
             }
+            System.out.println(count);
         }
-        System.out.println(count);
     }
-}
